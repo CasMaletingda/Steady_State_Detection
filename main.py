3efe605ebb62bd0de28b7a36ac00dc5f122f3606
CasMarletinda <jingchaosong2020@gmail.com>
2025-08-05 20:39:53 -0400

    读取文件并绘图

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-data = {
-    "Name": ["Alice", "Bob", "Charlie"],
-    "Age": [25, 30, 35]
-}
+# 读取Excel数据
+df = pd.read_excel('data.xlsx')
 
-df = pd.DataFrame(data)
+# 解析并转换时间格式
+if 'Time' in df.columns:
+    try:
+        df['Time'] = pd.to_datetime(df['Time'], unit='ms')
+    except Exception:
+        df['Time'] = pd.to_datetime(df['Time'])
 
-print(df)
+# 绘图
+for col in df.columns:
+    if col == 'Time':
+        continue
+    #定义绘图大小
+    plt.figure(figsize=(12, 6))
+    plt.title(col)
+    #取“Time"列作X轴，并取当前一列数据作y轴
+    plt.plot(df['Time'], df[col], color='skyblue')
+    plt.xlabel("Date_Time")
+    plt.ylabel("Value")
+    plt.grid(axis='y', linestyle='-')
+    plt.show()
